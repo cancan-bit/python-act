17a07aeb4c09d47ec6facc9f8af3a7972755bcc4
cancan <srijan.prasan@gmail.com>
2022-04-30 23:40:35 +0530

    created the context manager for mysql

---
+++ dbcm.py
@@ -0,0 +1,16 @@
+import mysql.connector as s
+
+class UseDatabase():
+    
+    def __init__(self, config):
+        self.config = config
+
+    def __enter__(self):
+        self.conn = s.connect(**config)
+        self.cur = self.conn.cursor()
+        return self.cur
+
+    def __exit__(self,exc_type,exc_value, exc_trace):
+        self.conn.commit()
+        self.cur.close()
+        self.conn.close()
