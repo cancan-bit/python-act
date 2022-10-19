01c07b0dd0e8bcc5b44c2b08b16696b7e125182b
cancan <srijan.prasan@gmail.com>
2022-10-19 20:25:36 +0530

    finalized scrollbar and past purchases

--- customers.py
+++ customers.py
@@ -2,10 +2,12 @@ import mysql.connector as s
 from dbcm import UseDatabase
 
 dbconfig = {'host':'localhost',
-            'user':'root',
-            'passwd':'admin',
-            'database':'12c', # <- check if database is correct
+            'user':'pythonact1',
+            'passwd':'python123',
+            'database': 'shop_info',
+            'port': 3308
         }
+
 
 def create_acc(phone_no, cust):
     with UseDatabase(dbconfig) as cur:
@@ -28,3 +30,16 @@ def modify_info(phone_no,pts):
             sql = "update customer_info set points = points + {} where phone_no= {}".format(pts,phone_no)
             cur.execute(sql)
 
+
+def purchase_data():
+    with UseDatabase(dbconfig) as cur:
+        sql = 'select max(bill_no) from shop_purchases;'
+        cur.execute(sql)
+        val = cur.fetchone()[0]
+        data = []
+        for i in range(1,val+1):
+            sql = 'select * from shop_purchases where bill_no = {}'.format(i)
+            cur.execute(sql)
+            info = cur.fetchall()
+            data.append(info)
+    return data
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 import mysql.connector as s
 from dbcm import UseDatabase
 from datetime import datetime
+from customers import *
+from math import log 
  
 root = Tk()
  
@@ -24,44 +27,65 @@ class App():
 
         self.total_val = DoubleVar()
 
+        self.purchase_hist = {}
 
- 
         self.dbconfig = {'host':'localhost',
                          'user':'pythonact1',
                          'passwd':'python123',
                          'database': 'shop_info',
                          'port': 3308
         }
+
         self.root = root
         self.i = 0
         self.data = []
+        self.c=1
+        self.err = 0
  
         self.purchase_data = []
         self.item_dict = dict()
         self.prices_dict = dict()
-        self.root.minsize(800,400)
- 
-        self.inputs = Frame(self.root)
+
+        self.main_frame = Frame(self.root)
+        self.canvas = Canvas(self.main_frame,background = '#caffbf')
+        self.new_frame = Frame(self.canvas,background='#b2bdff')
+        self.new_frame.grid(row=0,column=0, columnspan=2)
+        self.main_frame.pack(fill='both',expand=1)
+        self.canvas.pack(side='left',fill='both',expand=1)
+
+        self.inputs = Frame(self.new_frame,background = '#fdff86',padx=20,pady=20)
+        self.info = Frame(self.new_frame,background='#9bf6ff',padx=20,pady=20)
+
+        self.canvas.create_window((0,0),window=self.new_frame,anchor='nw')
+
+
         self.root.title('Cashier')
-        self.root.columnconfigure(0,weight=20)
-        self.root.columnconfigure(1,weight=1)
+        self.root.configure(background='#b2bdff')
+        self.canvas.columnconfigure(0,weight=20)
+        self.canvas.columnconfigure(1,weight=5)
  
-        Label(self.root, text='Welcome to the cashier app', font='Helvetica 18 bold').grid(row=0,column=0,pady=10)
+        Label(self.new_frame, text='Welcome to the cashier app', font='Helvetica 26 bold',background='#b2bdff').grid(row=0,column=0,pady=10)
  
-        self.inputs.columnconfigure(0,weight=1)
+        self.inputs.columnconfigure(0,weight=2)
         self.inputs.columnconfigure(1,weight=10)
-        self.inputs.columnconfigure(2,weight=1)
-        self.inputs.columnconfigure(3,weight=1)
-        self.inputs.columnconfigure(4,weight=1)
-        self.inputs.grid(row=1,column=0,sticky='nsew',padx=10)
+        self.inputs.columnconfigure(2,weight=2)
+        self.inputs.columnconfigure(3,weight=2)
+        self.inputs.columnconfigure(4,weight=2)
+        self.inputs.grid(row=1,column=0,sticky='ns',padx=10,pady=20)
  
  
-        Label(self.inputs,text='Item no.').grid(row=0,column=0,sticky='EW' )
-        Label(self.inputs,text='Item').grid(row=0,column=1,sticky='EW')
-        Label(self.inputs,text='Qty').grid(row=0,column=2,sticky='EW' )
-        Label(self.inputs,text='Price').grid(row=0,column=3,sticky='EW')
-        Label(self.inputs,text='Cost').grid(row=0,column=4,sticky='EW')
+        Label(self.inputs,text='Item no.', font = 'Calibri 20').grid(row=0,column=0,sticky='EW' )
+        Label(self.inputs,text='Item', font = 'Calibri 20').grid(row=0,column=1,sticky='EW')
+        Label(self.inputs,text='Qty', font = 'Calibri 20').grid(row=0,column=2,sticky='EW' )
+        Label(self.inputs,text='Price', font = 'Calibri 20').grid(row=0,column=3,sticky='EW')
+        Label(self.inputs,text='Cost', font = 'Calibri 20').grid(row=0,column=4,sticky='EW')
  
+        scroll = ttk.Scrollbar(self.main_frame, orient='vertical', command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=scroll.set)
+        scroll.pack(side='right',fill='y')
+        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")) )
+
+        
         with UseDatabase(self.dbconfig) as cur:
             sql = '''create table if not exists shop_prices(
                     itemno int primary key,
@@ -75,7 +99,9 @@ class App():
                           (2,'Detergent',250),
                           (3, 'Shampoo',340),
                           (4,'Face cream',460),
-                          (5,'Perfume',550);
+                          (5,'Perfume',550),
+                          (6,'Toothpaste',100),
+                          (7,'Hair Gel',230);
                        '''
                 cur.execute(sql)
             except:
@@ -108,12 +134,18 @@ class App():
         for i in self.data:
             self.item_dict[i[0]] = i[1]
             self.prices_dict[i[0]] = i[2]
- 
+
+        self.info.grid(row=2,column=0,padx=10,pady=20)
+        self.display()
+
         self.button_create()
         self.row_create()
  
     def fetch_item(self,event):
-        s = str(event.widget)[14:]   #.!frame.!entry(no)
+        if self.err != 0:
+                s = str(event.widget)[36+int(log(self.err,10)):]
+        else:
+            s = str(event.widget)[36:]
         print(event.widget)
         pos = int(s)//6
         print(pos)
@@ -130,7 +162,10 @@ class App():
  
     def fetch_cost(self,event):
         try:
-            s = str(event.widget)[14:]
+            if self.err != 0:
+                s = str(event.widget)[36+int(log(self.err,10)):]
+            else:
+                s = str(event.widget)[36:]
             pos = int(s)//6
             print(pos)
             if int(self.qty[pos].get()) >= 0:
@@ -163,25 +198,18 @@ class App():
         self.qty.append(None)
         self.price.append(None)
         self.cost.append(None)
-
-        '''if not first:
-            time = datetime.now()
-            push_time = time.strftime('%Y-%m-%d %H:%M:%S')
  
-            push_info = [int(self.itemno.get()), self.item.get(), int(self.qty.get()), float(self.price.get()), float(self.cost.get()),push_time]
-            self.purchase_data.append(push_info)'''
- 
-        self.itemno[self.i] = Entry(self.inputs,textvariable=self.itemno_var[self.i])
+        self.itemno[self.i] = Entry(self.inputs,textvariable=self.itemno_var[self.i],font =('Calibri 20'))
         self.itemno[self.i].grid(row=self.i+1, column=0,sticky='WE' )
         self.itemno[self.i].bind('<Tab>',self.fetch_item)
-        self.qty[self.i] = Entry(self.inputs,textvariable=self.qty_var[self.i])
+        self.qty[self.i] = Entry(self.inputs,textvariable=self.qty_var[self.i],font =('Calibri 20'))
         self.qty[self.i].grid(row=self.i+1, column=2,sticky='WE')
         self.qty[self.i].bind('<Tab>',self.fetch_cost)
-        self.item[self.i] = Entry(self.inputs,state='readonly',textvariable=self.item_var[self.i])
+        self.item[self.i] = Entry(self.inputs,state='readonly',textvariable=self.item_var[self.i],font =('Calibri 20'))
         self.item[self.i].grid(row=self.i+1, column=1,sticky='WE' )
-        self.price[self.i] = Entry(self.inputs,state='readonly',textvariable=self.price_var[self.i])
+        self.price[self.i] = Entry(self.inputs,state='readonly',textvariable=self.price_var[self.i],font =('Calibri 20'))
         self.price[self.i].grid(row=self.i+1, column=3,sticky='WE')
-        self.cost[self.i] = Entry(self.inputs,state='readonly',textvariable=self.cost_var[self.i])
+        self.cost[self.i] = Entry(self.inputs,state='readonly',textvariable=self.cost_var[self.i],font =('Calibri 20'))
         self.cost[self.i].grid(row=self.i+1,column=4,sticky='WE')
  
         self.enter.destroy()
@@ -198,24 +226,24 @@ class App():
  
     def button_create(self,event=None):
  
-        self.enter = Button(self.inputs, text='Save')
+        self.enter = Button(self.inputs, text='Save', font =('Calibri 20'))
         self.enter.bind('<Button-1>',self.save)
         self.enter.grid(row=self.i+3, column=0, sticky='w',pady=10)
  
-        self.next = Button(self.inputs, text='Add')
+        self.next = Button(self.inputs, text='Add', font =('Calibri 20'))
         self.next.bind('<Button-1>',self.row_create)
         self.next.grid(row=self.i+3, column=2,pady=10)
  
-        self.disp_price = Button(self.inputs, text='Prices')
-        self.disp_price.bind('<Button-1>',self.display)
+        self.disp_price = Button(self.inputs, text='Past Purchases', font =('Calibri 20'))
+        self.disp_price.bind('<Button-1>',self.purchases)
         self.disp_price.grid(row=self.i+3, column=4, sticky='e',pady=10)
  
-        self.total_label = Label(self.inputs,text='Total')
+        self.total_label = Label(self.inputs,text='Total', font = 'Calibri 20')
         self.total_label.grid(row=self.i+4,column=0,sticky='W')
-        self.total = Entry(self.inputs,state='readonly',textvariable=self.total_val)
+        self.total = Entry(self.inputs,state='readonly',textvariable=self.total_val,font =('Calibri 20'))
         self.total.grid(row=self.i+4,column=1,sticky='WE',columnspan=4)
  
-        self.end = Button(self.inputs,text='Quit' )
+        self.end = Button(self.inputs,text='Quit', font =('Calibri 20'))
         self.end.grid(row=self.i+5,column=2,pady=10)
         self.end.bind('<Button-1>',self.quit)
  
@@ -243,28 +271,99 @@ class App():
                             ({},{},'{}',{},{},{},'{}')'''.format(bill_no,row[0],row[1],row[2],row[3],row[4],row[5])
                         cur.execute(sql)
             self.purchase_data = list()
+            self.main_frame.destroy()
+            self.err += 1
             self.__init__(self.root)
         else:
             messagebox.showwarning('Null value','Please enter some values')
  
-    def display(self,event=None):
-        r_count = 1
-        c_count = 0
-        self.info = Toplevel(self.root)
-        self.info.title('Prices')
-        self.info.geometry('300x300')
- 
-        Label(self.info,text='Item no.',font='Helvetica 18 bold').grid(row=0,column=0,sticky='ew')
-        Label(self.info,text='Item',font='Helvetica 18 bold').grid(row=0,column=1,sticky='ew')
-        Label(self.info,text='Price',font='Helvetica 18 bold').grid(row=0,column=2,sticky='ew')
- 
+    def display(self):
+
+        head = ('Item_no.','Item_name','Price') 
+        
+        tree = ttk.Treeview(self.info,columns=head, show='headings')
+
+        style = ttk.Style(self.info)
+        style.configure('Treeview',font=('Calibri 20'),rowheight='30',padx=10)
+        style.configure('Treeview.Heading',font=('Arial 23 bold'),rowheight='27')
+
+        tree.heading('Item_no.',text='Item No.')
+        tree.heading('Item_name', text='Item Name')
+        tree.heading('Price', text='Price')
+
         for row in self.data:
-            for val in row:
-                Label(self.info,text=val,font='Helvetica 15').grid(row=r_count,column=c_count,sticky='ew')
-                c_count += 1
-            r_count+=1
-            c_count=0
- 
+            tree.insert('', END,values=row)
+
+        tree.grid(row=0,column=0,sticky='ew')
+
+    def purchases(self,event=None):
+        values= purchase_data()
+        self.max_bno = len(values)
+        for i in values:
+            self.purchase_hist[int(i[0][0])] = i
+
+
+        self.win = Toplevel()
+        self.win.configure(background='#b2bdff')
+        self.create_plist(self.win)
+   
+        self.win.mainloop()
+
+    def create_plist(self,w):
+
+        try:
+            self.tree.destroy()
+            self.prev.destroy()
+            self.next.destroy()
+        except:
+            pass
+
+        head = ('bill_no','i_no','i_name','qty','price','cost')
+        
+        self.tree = ttk.Treeview(w, columns=head, show='headings')
+
+        style = ttk.Style(w)
+        style.configure('Treeview',font=('Calibri 20'),rowheight='30',padx=10)
+        style.configure('Treeview.Heading',font=('Arial 23 bold'),rowheight='27')
+
+        self.tree.heading('bill_no',text='Bill No.')
+        self.tree.heading('i_no', text='Item No.')
+        self.tree.heading('i_name', text='Item')
+        self.tree.heading('qty',text='Qty')
+        self.tree.heading('price',text='Price')
+        self.tree.heading('cost',text='Cost')
+
+        run_tot = 0
+        for row in self.purchase_hist[self.c]:
+            run_tot += int(row[-2])
+            self.tree.insert('',END,values=row)
+
+        self.tree.grid(row=0,column=0,columnspan=2)
+
+        Label(w, text='Total', font='Helvetica 26 bold',background='#b2bdff').grid(row=1,column=0)
+        Label(w, text=str(run_tot),font='Helvetica 26 bold',background='#b2bdff').grid(row=1,column=1)
+
+        self.prev = Button(w, text='Previous',font=('Calibri 20'))
+        self.prev.bind('<Button-1>', self.increment)
+        self.prev.grid(row=2,column=0)
+     
+        self.next = Button(w, text='Next',font=('Calibri 20'))
+        self.next.bind('<Button-1>', self.decrement)
+        self.next.grid(row=2,column=1)
+
+
+    def increment(self, event=None):
+        if self.c > 1:
+            self.c -= 1
+            self.create_plist(self.win)
+
+
+    def decrement(self,event=None):
+        if self.c < self.max_bno:
+            self.c += 1 
+            self.create_plist(self.win)
+
+
     def quit(self,event=None):
         self.root.destroy()
  
